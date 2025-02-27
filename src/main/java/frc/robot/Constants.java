3c570b6914d70fc1ddc8d5b0198ea5c1d467d855
MasonLu0504 <masonlu1806@gmail.com>
2025-02-27 13:23:45 -0600

    Elevator Code
    Not implemented in RobotContainer yet

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,15 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 
 public final class Constants {
 
@@ -22,23 +31,39 @@ public final class Constants {
         public static final int elevatorFollowerCan = 14;
     }
 
-    //measurements in inches
     public static class ElevatorConstants {
-        public static final double minExtension = 0;
-        public static final double maxExtension = 72;
-        public static final double l2Setpoint = 24;
-        public static final double l3Setpoint = 40;
-        public static final double l4Setpoint = 54;
-        public static final double intakeSetpoint = 12;
+        public static final double kElevatorGearing = 25;
+        public static final double kSprocketTeeth = 22;
+        public static final double kSprocketPitch = Units.inchesToMeters(1.76);
+        public static final double kCarriageMass = 20;
+        public static final double minExtension = Units.inchesToMeters(0);
+        public static final double maxExtension = Units.inchesToMeters((72 - 16) / 2);
+        public static final Distance l2Setpoint = Inches.of(24);
+        public static final Distance l3Setpoint = Inches.of(40);
+        public static final Distance l4Setpoint = Inches.of(54);
+        public static final Distance intakeSetpoint = Inches.of(12);
+        public static final double kElevatorKp = 30;
+        public static final double kElevatorKi = 0;
+        public static final double kElevatorKd = 10;
+        public static final double kElevatorkS = 0.2;
+        public static final double kElevatorkV = 21.8;
+        public static final double kElevatorkA = 0.14;
+        public static final double kElevatorkG = 0.87;
+        public static final double kMaxVelocity = Meters.of(1).per(Second).in(MetersPerSecond);
+        public static final double kMaxAccel = Meters.of(0.5).per(Second).per(Second).in(MetersPerSecondPerSecond);
+        public static final int kElevatorMaxCurrent = 40;
+        public static final double kElevatorRampRate = 0.1;
+        public static final Distance kElevatorTolerance = Inches.of(1/32);
+        public static final Distance kScoreDrop = Inches.of(6);
     }
 
     //measurements in degrees, where rotating up/counterclockwise is positive
     public static class EndEffectorConstants {
-        public static final double startingAngle = 60;
-        public static final double restingAngle = 0;
-        public static final double clearanceAngle = 25;
-        public static final double intakeAngle = 20;
-        public static final double wristHorizontalAngle = 0;
-        public static final double wristVerticalAngle = 90;
+        public static final Angle startingAngle = Degrees.of(60);
+        public static final Angle restingAngle = Degrees.of(0);
+        public static final Angle clearanceAngle = Degrees.of(25);
+        public static final Angle intakeAngle = Degrees.of(20);
+        public static final Angle wristHorizontalAngle = Degrees.of(0);
+        public static final Angle wristVerticalAngle = Degrees.of(90);
     }
 }
---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,141 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.Meters;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.CanId;
+import frc.robot.Constants.ElevatorConstants;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    //intialize motors
+    private final SparkMax elevatorMotor = new SparkMax(CanId.elevatorMotorCan, MotorType.kBrushless);
+    private final SparkMax elevatorFollower = new SparkMax(CanId.elevatorFollowerCan, MotorType.kBrushless);
+
+    //intitalize relative encoder based on the main motor
+    private final RelativeEncoder elevatorEncoder = elevatorMotor.getEncoder();
+
+    private final ProfiledPIDController elevatorPid = 
+        new ProfiledPIDController(
+            ElevatorConstants.kElevatorKp,
+            ElevatorConstants.kElevatorKi,
+            ElevatorConstants.kElevatorKd,
+            new Constraints(
+                ElevatorConstants.kMaxVelocity,
+                ElevatorConstants.kMaxAccel
+            )
+        );
+
+    private final ElevatorFeedforward elevatorFeed = 
+        new ElevatorFeedforward(
+            ElevatorConstants.kElevatorkS,
+            ElevatorConstants.kElevatorkG,
+            ElevatorConstants.kElevatorkV,
+            ElevatorConstants.kElevatorkA
+        );
+
+    public final Trigger minStop = 
+        new Trigger(() -> MathUtil.isNear(
+            getHeightInches(),
+            ElevatorConstants.minExtension,
+            1
+            )
+        );
+    
+    public final Trigger maxStop = 
+        new Trigger(() -> MathUtil.isNear(
+            getHeightInches(),
+            ElevatorConstants.maxExtension,
+            1
+            )
+        );
+
+    public ElevatorSubsystem() {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.idleMode(IdleMode.kCoast)
+        .smartCurrentLimit(ElevatorConstants.kElevatorMaxCurrent)
+        .closedLoopRampRate(ElevatorConstants.kElevatorRampRate);
+
+        elevatorMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+
+        SparkMaxConfig followerConfig = new SparkMaxConfig();
+        followerConfig.idleMode(IdleMode.kCoast)
+        .smartCurrentLimit(ElevatorConstants.kElevatorMaxCurrent)
+        .closedLoopRampRate(ElevatorConstants.kElevatorRampRate)
+        .follow(elevatorMotor, true);
+
+        elevatorFollower.configure(followerConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public double getHeightInches() {
+        return 
+        (elevatorEncoder.getPosition() / ElevatorConstants.kElevatorGearing)
+        * (2 * Math.PI * ElevatorConstants.kSprocketPitch);
+    }
+
+    public double getVelocityInchesPerSecond() {
+        return ((elevatorEncoder.getVelocity() / 60) / ElevatorConstants.kElevatorGearing)
+        * (2 * Math.PI * ElevatorConstants.kSprocketPitch);
+    }
+
+    public void reachHeight(double height) {
+        double voltsOut = MathUtil.clamp(
+            elevatorPid.calculate(getHeightInches(), height) +
+            elevatorFeed.calculateWithVelocities(
+                getVelocityInchesPerSecond(),
+                elevatorPid.getSetpoint().velocity
+            ),
+            -12, 12 
+        );
+
+        elevatorMotor.setVoltage(voltsOut);
+    }
+
+    public Command setHeight(double height) {
+        return run(() -> reachHeight(height));
+    }
+
+    public void stop() {
+        elevatorMotor.set(0.0);
+    }
+
+    public Command stopElevator() {
+        return run(() -> stop());
+    }
+
+    public boolean aroundHeight(double height, double tolerance) {
+        return MathUtil.isNear(
+            height, 
+            getHeightInches(), 
+            ElevatorConstants.kElevatorTolerance.in(Inches)
+        );
+    }
+
+    private double holdPoint = 0;
+    public Command holdPosition() {
+        return startRun(
+            () -> {
+                holdPoint = getHeightInches();
+                elevatorPid.reset(holdPoint);
+            },
+            () -> {
+                reachHeight(holdPoint);
+            }
+        );
+    }
+}

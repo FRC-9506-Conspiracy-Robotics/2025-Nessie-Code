d1cb658c5848c6411931f46ed5f46d09e24f12cb
MasonLu0504 <masonlu1806@gmail.com>
2025-02-26 21:04:57 -0600

    Swerve Code Rework
    Still two errors in RobotContainer.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+public final class Constants {
+    public static class DriverConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final double kDeadband = 0.05;
+    }
+    public static class SwerveConstants {
+        public static final double maxSpeed = Units.feetToMeters(15);
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,95 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.DriverConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import java.io.File;
+import swervelib.SwerveInputStream;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+    final CommandXboxController mDriverController = new CommandXboxController(DriverConstants.kDriverControllerPort);
+    private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
-  private void configureBindings() {}
+      /**
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
+   */
+    SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                () -> mDriverController.getLeftY() * -1,
+                                                                () -> mDriverController.getLeftX() * -1)
+                                                            .withControllerRotationAxis(mDriverController::getRightX)
+                                                            .deadband(DriverConstants.kDeadband)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+  /**
+   * Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+   */
+    SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(mDriverController::getRightX,
+                                                                                             mDriverController::getRightY)
+                                                           .headingWhile(true);
+
+  /**
+   * Clone's the angular velocity input stream and converts it to a robotRelative input stream.
+   */
+    SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+                                                             .allianceRelativeControl(false);
+
+    SwerveInputStream driveAngularVelocityKeyboard = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                        () -> -mDriverController.getLeftY(),
+                                                                        () -> -mDriverController.getLeftX())
+                                                                    .withControllerRotationAxis(() -> mDriverController.getRawAxis(
+                                                                        2))
+                                                                    .deadband(DriverConstants.kDeadband)
+                                                                    .scaleTranslation(0.8)
+                                                                    .allianceRelativeControl(true);
+  // Derive the heading axis with math!
+    SwerveInputStream driveDirectAngleKeyboard     = driveAngularVelocityKeyboard.copy()
+                                                                               .withControllerHeadingAxis(() ->
+                                                                                                              Math.sin(
+                                                                                                                  mDriverController.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2),
+                                                                                                          () ->
+                                                                                                              Math.cos(
+                                                                                                                  mDriverController.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2))
+                                                                               .headingWhile(true);
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {
+        Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+        Command driveFieldOrientedAngularVelocityKeyboard = drivebase.driveFieldOriented(driveAngularVelocity);
+        if (RobotBase.isSimulation()) {
+            drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+        } else {
+            drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+        }
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
